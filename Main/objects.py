de972888beedb46b49465d2699e5b3d0c4c01b27
Davyd_Khko <dastermastergg@gmail.com>
2026-06-24 22:08:44 +0200

    czytelnosc object py

--- Main/objects.py
+++ Main/objects.py
@@ -1,4 +1,5 @@
 class Player:
+    # Konstruktor gracza - ładowanie tekstur (sprajtów) i ustawienie statystyk
     def __init__(self, image_path, open_image_path, x, y, w, h):
         self.img_closed = loadImage(image_path)
         self.img_open = loadImage(open_image_path)
@@ -15,18 +16,21 @@ class Player:
         self.flip_x = False
         self.eating_timer = 0
 
+        # System żyć i klatek niezniszczalności
         self.hp = 3
         self.invulnerable_timer = 0
 
+        # Flagi kierunkowe do płynnego ruchu
         self.left = False
         self.right = False
         self.up = False
         self.down = False
 
+    #Rejestracja wciśnięcia klawisza
     def press_key(self, k):
         if k == 'a' or k == 'A':
             self.left = True
-            self.flip_x = True
+            self.flip_x = True # Zaznaczenie, że tekstura powinna zostać obrócona
         elif k == 'd' or k == 'D':
             self.right = True
             self.flip_x = False
@@ -35,6 +39,7 @@ class Player:
         elif k == 's' or k == 'S':
             self.down = True
 
+    #Rejestracja puszczenia klawisza (zatrzymanie ruchu w danej osi)
     def release_key(self, k):
             if k == 'a' or k == 'A':
                 self.left = False
@@ -45,7 +50,9 @@ class Player:
             elif k == 's' or k == 'S':
                 self.down = False
     
+    #Aktualizacja stanu co klatkę
     def update(self, level, cell_w, cell_h):
+        # Zmniejszanie licznika niezniszczalności
         if self.invulnerable_timer > 0:
             self.invulnerable_timer -= 1
 
@@ -65,6 +72,7 @@ class Player:
         new_x = self.x + dx
         new_y = self.y + dy
 
+        # Niezależne sprawdzanie kolizji dla osi X i Y, co pozwala na płynne przesuwanie się wzdłuż ścian
         if level.can_move_to(new_x, self.y, self.w, self.h, cell_w, cell_h):
             self.x = new_x
 
@@ -73,11 +81,16 @@ class Player:
 
         if self.eating_timer > 0:
             self.eating_timer -= 1
-
+            
+            
+    # Aktywacja animacji jedzenia
     def eat(self):
         self.eating_timer = 20
 
+    # System renderowania i wizualizacji gracza
     def display(self):
+        
+        #Migotanie gracza za pomocą operacji modulo podczas bycia niezniszczalnym
         if self.invulnerable_timer > 0 and (self.invulnerable_timer / 10) % 2 == 0:
             return
 
@@ -86,16 +99,19 @@ class Player:
         else:
             current_img = self.img_closed
 
+        # Służą do dynamicznego obracania tekstury bez konieczności ładowania osobnych plików graficznych
         pushMatrix()
         translate(self.x, self.y)
 
         if self.flip_x:
-            scale(-1, 1)
+            scale(-1, 1) # Odbicie lustrzane na osi X
 
         image(current_img, 0, 0, self.w, self.h)
 
         popMatrix()
+     
         
+    # Resetowanie stanu po utracie życia   
     def reset_position(self):
         self.x = self.start_x
         self.y = self.start_y
@@ -105,7 +121,7 @@ class Player:
         self.up = False
         self.down = False
     
-
+# Klasa bazowa duchów -  Zawiera wspólne cechy dziedziczone przez konkretne typy duchów
 class Ghost(object):
     def __init__(self, x, y, w, h, image_path):
         self.x = x
@@ -118,7 +134,7 @@ class Ghost(object):
     def display(self):
         image(self.img, self.x, self.y, self.w, self.h)
 
-
+#Porusza się w jednym kierunku, aż uderzy w ścianę
 class RandomGhost(Ghost):
     def __init__(self, x, y, w, h):
         Ghost.__init__(self, x, y, w, h, "RandomGhost.png")
@@ -139,25 +155,29 @@ class RandomGhost(Ghost):
 
         new_x = self.x + dx
         new_y = self.y + dy
-
+    
+        # Jeśli może iść - idzie. Jeśli napotka ścianę - losuje nowy kierunek
         if level.can_move_to(new_x, new_y, self.w, self.h, cell_w, cell_h):
             self.x = new_x
             self.y = new_y
         else:
             self.direction = int(random(4))
 
-
+# Matematycznie wylicza wektor do gracza i go ściga
 class HunterGhost(Ghost):
     def __init__(self, x, y, w, h):
         Ghost.__init__(self, x, y, w, h, "HunterGhost.png")
-
+        
+    # Obliczanie różnicy dystansu na obu osiach
     def move(self, pacman_x, pacman_y, level, cell_w, cell_h):
         diff_x = pacman_x - self.x
         diff_y = pacman_y - self.y
 
+        # Ustalanie kierunku (wektora) ruchu
         move_x = self.speed if diff_x > 0 else -self.speed if diff_x < 0 else 0
         move_y = self.speed if diff_y > 0 else -self.speed if diff_y < 0 else 0
 
+        #Duch wybiera ruch w osi, w której ma dalej do gracza
         if abs(diff_x) > abs(diff_y):
             primary_axis = 'x'
             primary_move = move_x
@@ -170,7 +190,8 @@ class HunterGhost(Ghost):
             secondary_move = move_x
 
         moved = False
-
+        
+        # Próba ruchu na osi priorytetowej
         if primary_axis == 'x' and primary_move != 0:
             if level.can_move_to(self.x + primary_move, self.y, self.w, self.h, cell_w, cell_h):
                 self.x += primary_move
@@ -181,6 +202,8 @@ class HunterGhost(Ghost):
                 self.y += primary_move
                 moved = True
 
+        # Mechanizm omijania przeszkód: 
+        # Jeśli ruch na głównej osi jest zablokowany, duch próbuje obejść ścianę używając osi drugorzędnej
         if not moved:
             if primary_axis == 'x':
                 if secondary_move == 0:
